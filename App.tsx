1a1bb97513c9cb16c1f0f176923750dc01764f86
DylanMA1 <dymolina@estudiantec.cr>
2024-11-05 22:11:22 -0600

    Maso

--- App.tsx
+++ App.tsx
@@ -12,7 +12,8 @@ import {
   ScrollView,
   HStack,
   VStack,
-  SimpleGrid,
+  Box,
+  useToast,
 } from "native-base";
 import { getSeats, checkAvailability } from "./hooks/useDatabase";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -43,6 +44,7 @@ export default function App() {
   const [quantity, setQuantity] = useState<string>("");
   const [availability, setAvailability] = useState<any>([]);
   const [selectedSeats, setSelectedSeats] = useState<Set<number>>(new Set());
+  const toast = useToast();
 
   useEffect(() => {
     const fetchSeats = async () => {
@@ -61,6 +63,18 @@ export default function App() {
       }
       const availableZones = await checkAvailability(category, numericQuantity);
       setAvailability(availableZones);
+
+      toast.show({
+        title: "Zonas recomendadas",
+        description: availableZones
+          .map(
+            (zone: any) =>
+              `Zona: ${zone.zone}, Asientos disponibles: ${zone.available_seats}`
+          )
+          .join("\n"),
+        variant: "solid",
+        duration: 5000,
+      });
     }
   };
 
@@ -90,7 +104,14 @@ export default function App() {
       }
       return blocks;
     };
-  
+
+    const getZoneBackgroundColor = (zoneName: string, categoryName: string) => {
+      const availableZone = availability.find(
+        (zone: any) => zone.zone === zoneName && category === categoryName
+      );
+      return availableZone ? "lightgreen" : "gray.200";
+    };
+
     return seats.map((category) => (
       <ScrollView
         key={category.id}
@@ -99,13 +120,16 @@ export default function App() {
         contentContainerStyle={{ flexGrow: 1 }}
         showsHorizontalScrollIndicator={false}
       >
-          {category.zones.map((zone) => (
-            <View key={zone.id} style={{ marginVertical: 5 }}>
-              <Text fontSize="md" fontWeight="semibold" color="gray.500">
-                Zona: {zone.name}
-              </Text>
-  
-              {category.name === "VIP" ? (
+        {category.zones.map((zone) => (
+          <View key={zone.id} style={{ marginVertical: 5 }}>
+            {category.name === "VIP" ? (
+              <Box
+                bgColor={getZoneBackgroundColor(zone.name, "VIP")}
+                padding={1}
+              >
+                <Text fontSize="md" fontWeight="semibold" color="gray.500">
+                  Zona: {zone.name}
+                </Text>
                 <HStack space={3}>
                   {getSeatBlocks(zone.seats, false).map((block, blockIndex) => (
                     <VStack
@@ -127,7 +151,7 @@ export default function App() {
                                 : isSelected
                                 ? "green.500"
                                 : "yellow.600";
-  
+
                               return (
                                 <Button
                                   key={seat.id}
@@ -146,54 +170,82 @@ export default function App() {
                     </VStack>
                   ))}
                 </HStack>
-              ) : (
-                <SimpleGrid columns={2} space={0}>
-                  {getSeatBlocks(zone.seats, true).map((block, blockIndex) => (
-                    <VStack
-                      key={blockIndex}
-                      space={2}
-                      padding={2}
-                      borderRadius="md"
-                    >
-                      {Array.from({ length: 4 }).map((_, rowIndex) => (
-                        <HStack key={rowIndex} space={1}>
-                          {block
-                            .slice(rowIndex * 5, rowIndex * 5 + 5)
-                            .map((seat: Seat) => {
-                              const isSelected = selectedSeats.has(seat.id);
-                              const seatBgColor = seat.sold
-                                ? "red.500"
-                                : seat.reserved
-                                ? "yellow.400"
-                                : isSelected
-                                ? "green.500"
-                                : "brown";
-  
-                              return (
-                                <Button
-                                  key={seat.id}
-                                  width={10}
-                                  height={10}
-                                  bg={seatBgColor}
-                                  isDisabled={seat.sold || seat.reserved}
-                                  onPress={() => toggleSeatSelection(seat.id)}
-                                >
-                                  {seat.id}
-                                </Button>
-                              );
-                            })}
-                        </HStack>
-                      ))}
-                    </VStack>
-                  ))}
-                </SimpleGrid>
-              )}
-            </View>
-          ))}
+              </Box>
+            ) : (
+              <>
+                <Box
+                  width="100%"
+                  bg="green.500"
+                  height={400}
+                  justifyContent="center"
+                  alignItems="center"
+                >
+                  <Text color="white">Estadio</Text>
+                </Box>
+                <Box
+                  bgColor={getZoneBackgroundColor(zone.name, "General")}
+                  padding={1}
+                >
+                  <Text
+                    fontSize="md"
+                    fontWeight="semibold"
+                    color="gray.500"
+                    marginLeft={2}
+                  >
+                    Zona: {zone.name}
+                  </Text>
+                  <HStack space={1}>
+                    {getSeatBlocks(zone.seats, true).map(
+                      (block, blockIndex) => (
+                        <VStack
+                          space={2}
+                          padding={2}
+                          borderRadius="md"
+                          key={blockIndex}
+                        >
+                          {Array.from({ length: 4 }).map((_, rowIndex) => (
+                            <HStack key={rowIndex} space={1}>
+                              {block
+                                .slice(rowIndex * 5, rowIndex * 5 + 5)
+                                .map((seat: Seat) => {
+                                  const isSelected = selectedSeats.has(seat.id);
+                                  const seatBgColor = seat.sold
+                                    ? "red.500"
+                                    : seat.reserved
+                                    ? "yellow.400"
+                                    : isSelected
+                                    ? "green.500"
+                                    : "brown";
+
+                                  return (
+                                    <Button
+                                      key={seat.id}
+                                      width={10}
+                                      height={10}
+                                      bg={seatBgColor}
+                                      isDisabled={seat.sold || seat.reserved}
+                                      onPress={() =>
+                                        toggleSeatSelection(seat.id)
+                                      }
+                                    >
+                                      {seat.id}
+                                    </Button>
+                                  );
+                                })}
+                            </HStack>
+                          ))}
+                        </VStack>
+                      )
+                    )}
+                  </HStack>
+                </Box>
+              </>
+            )}
+          </View>
+        ))}
       </ScrollView>
     ));
   };
-  
 
   return (
     <NativeBaseProvider>
@@ -225,17 +277,6 @@ export default function App() {
               Verificar Disponibilidad
             </Button>
           </VStack>
-
-          {availability.length > 0 && (
-            <View style={{ marginVertical: 10 }}>
-              {availability.map((zone: any, index: any) => (
-                <Text key={index} color="green.500">
-                  Zona: {zone.zone}, Asientos disponibles:{" "}
-                  {zone.available_seats}
-                </Text>
-              ))}
-            </View>
-          )}
 
           {renderSeats()}
 
